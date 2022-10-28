2318ba3e4e5964596211fdb2cdc6efbb928dfb37
ladew222 <egweinbe@gmail.com>
2022-10-28 11:28:40 -0500

    cleaned up

--- Skinner.py
+++ Skinner.py
@@ -52,7 +52,7 @@ class App:
         
         #create output video
         # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-        self.out = cv2.VideoWriter('/videos/output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+        self.out = cv2.VideoWriter('videos/output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
         
  
         # Create a canvas that can fit the above video souCrce size
@@ -92,7 +92,7 @@ class App:
         self.window.mainloop()
         
         
-     def do_motion():
+     def do_motion(self):
         print("motion")
     
      def serial_ports():    
